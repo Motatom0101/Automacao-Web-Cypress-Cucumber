41db2064f228ef9eb22ddbe73a4417aea6e29769
Tiago Mota <tmota.seg@gmail.com>
2025-11-13 01:45:37 -0300

    Transformando uma automação simples em modelo POM.

---
+++ cypress/e2e/elements/login.js
@@ -0,0 +1,6 @@
+
+class loginElements{
+    inputEmail = () => { return 'input[type="email"]'}
+    inputPass = () => { return 'input[type="password"]'}
+    btnEnter = () => { return 'Entrar'}
+} export default loginElements;
---
+++ cypress/e2e/pages/login_pages.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress"/>
+
+// 1. O Import da CLASSE (Nome original mantido: LoginElements)
+import loginElements from '../elements/login.js'
+// 2. A Instanciação da classe 'loginElements', para criar do objeto 'el'
+const el = new loginElements;// <- RECOMENDADO: Nome curto e descritivo
+
+// Criação da classe 'loginPage' para herdar os elementos e criar os métodos de interação
+class loginPage{
+
+    // Acessando a página e fazendo login
+    acessarHomePage(){
+        cy.visit('https://example.com/login')
+    }
+    // Preenchendo os dados de login
+    preencherDadosNaTela(){
+        cy.get(el.inputEmail).type('user@example.com')
+        cy.get(el.inputPass).type('senha123')
+    }
+    // Clicando no botão entrar
+    clicarNoBotaoEntrar(){
+        cy.get(el.btnEnter).click()
+    }
+}
